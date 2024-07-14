60bfa1d9a88abacaaaa9a92a2e04416334b5be2f
Chromiumore <parts.dima06@gmail.com>
2024-07-14 23:18:38 +0300

    echo bot

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-from conf import conf_tocken
+from conf import conf_token
+import telebot
+
+TOKEN = conf_token
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler()
+def echo(message):
+    bot.send_message(message.chat.id, message.text)
+
+bot.infinity_polling()
