9b7862397f068bbb9f0c5371ede2dbd7f36a46ef
Chromiumore <parts.dima06@gmail.com>
2024-07-16 01:27:21 +0300

    simple menu and commands

--- main.py
+++ main.py
@@ -5,8 +5,17 @@ TOKEN = conf_token
 
 bot = telebot.TeleBot(TOKEN)
 
-@bot.message_handler()
-def echo(message):
-    bot.send_message(message.chat.id, message.text)
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, "Привет! Добро пожаловать в лучший чат для знакомств.")
+    main_menu(message)
+
+@bot.message_handler(commands=['menu'])
+def main_menu(message):
+    markup = telebot.types.InlineKeyboardMarkup()
+    markup.add(telebot.types.InlineKeyboardButton('Редактировать анкету', callback_data='edit_form'))
+    markup.add(telebot.types.InlineKeyboardButton('Смотреть анкеты', callback_data='view_form'))
+    markup.add(telebot.types.InlineKeyboardButton('Настройки поиска', callback_data='search_settings'))
+    bot.send_message(message.chat.id, 'Выбери действие:  ', reply_markup=markup)
 
 bot.infinity_polling()
