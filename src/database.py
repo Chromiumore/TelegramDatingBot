e88ab424d3e69f4549b9034dfd6131e87170a9ec
Chromiumore <parts.dima06@gmail.com>
2024-08-05 18:15:02 +0300

    database: closing connection in __del__, no checking same thread

--- src/database.py
+++ src/database.py
@@ -4,9 +4,9 @@ from form import Form
 class Database:
     def __init__(self, file):
         self.file = file
-        connection = sqlite3.connect(self.file)
-        cursor = connection.cursor()
-        cursor.execute('''
+        self.connection = sqlite3.connect(self.file, check_same_thread=False)
+        self.cursor = self.connection.cursor()
+        self.cursor.execute('''
                         CREATE TABLE IF NOT EXISTS forms (
                         id   INTEGER PRIMARY KEY
                         UNIQUE
@@ -18,7 +18,7 @@ class Database:
                         );'''
         )
 
-        cursor.execute('''
+        self.cursor.execute('''
                        CREATE TABLE IF NOT EXISTS photos (
                        photo_id TEXT    PRIMARY KEY
                        UNIQUE
@@ -27,59 +27,51 @@ class Database:
                        NOT NULL
                        );''')
 
-        connection.commit()
-        cursor.close()
-        connection.close()
-        
+        self.connection.commit()
+
+    def __del__(self):
+        self.cursor.close()
+        self.connection.close()
+
     def upload_form(self, form : Form):
-        connection = sqlite3.connect(self.file)
-        cursor = connection.cursor()
         id = form.id
         name = form.name
         age = form.age
         sex = form.sex
         desc = form.desc
         photos = form.photos
-        cursor.execute(f'''
+        self.cursor.execute(f'''
                        DELETE FROM forms WHERE id = {id};
                        ''')
-        cursor.execute(f'''
+        self.cursor.execute(f'''
                        DELETE FROM photos WHERE user_id = {id};
                        ''')
-        cursor.execute(f'''
+        self.cursor.execute(f'''
                        INSERT INTO forms (id, name, age, sex, desc)
                        VALUES({id}, '{name}', {age}, '{sex}', '{desc}');
                        ''')
         for i in range(len(photos)):
-            cursor.execute(f'''
+            self.cursor.execute(f'''
                            INSERT INTO photos (photo_id, user_id)
                            VALUES('{photos[i]}', '{id}');
                            ''')
-        connection.commit()
-        cursor.close()
-        connection.close()
+        self.connection.commit()
             
     def download_data(self, id : int):
-        connection = sqlite3.connect(self.file)
-        cursor = connection.cursor()
-        cursor.execute(f'''
+        self.cursor.execute(f'''
                        SELECT * FROM forms
                        WHERE id = {id}
                        ''')
-        res = cursor.fetchone()
-        connection.commit()
-        cursor.close()
-        connection.close()
+        res = self.cursor.fetchone()
+        self.connection.commit()
         return res
     
     def download_photos(self, id : int):
-        connection = sqlite3.connect(self.file)
-        cursor = connection.cursor()
-        cursor.execute(f'''
+        self.cursor.execute(f'''
                        SELECT photo_id FROM photos
                        WHERE user_id = {id}
                        ''')
-        req = cursor.fetchall()
+        req = self.cursor.fetchall()
         photos = [x[0] for x in req]
         return photos
 
